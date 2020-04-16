7144bff99f3c73384f3a4c537b6cb32122896239
damian <Mechromancer1!>
2020-04-16 15:16:09 +0200

    bez folderu .idea

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,6 +2,7 @@ function start() {
 
 let button = document.getElementById('add_balls');
 button.addEventListener('click',randomballs);
+button.addEventListener('click',ballcheck);
 
  function randomballs(){
 
@@ -13,6 +14,14 @@ button.addEventListener('click',randomballs);
        let randomcolor = Math.floor(Math.random()*(5)+1);
        ballfield.classList.replace('field',`color${randomcolor}`);
    }
+ }
+ function ballcheck() {
+     let reds = document.getElementsByClassName('color1');
+     let len = Object.keys(reds).length;
+     for (let i=1;i<len+1;i++){
+
+     }
+
  }
 }
 
